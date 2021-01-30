9c0981f8cf73145d79779f8e1005604bba939866
Rrego6 <raoulrego@gmail.com>
2021-01-30 17:03:09 -0600

    Fix parsing of newegg

--- main.js
+++ main.js
@@ -2,17 +2,41 @@ const puppeteer = require('puppeteer');
 
 let url = 'https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664#';
 const outOfStockDivSelector = 'div.product-wrap div.product-inventory';
+const titleSelector = 'div.product-main div.product-wrap h1.product-title';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(url);
+
+  //get title
+  try {
+    let title = await page.$eval(
+      titleSelector,
+      (el) => {
+        return el.textContent;
+      }
+    );
+    if(title) {
+      title = title.trim();
+      console.log(title);
+    }
+  }
+  catch(e) {
+        console.log('error with retrieving title!')
+  }
   
   try {
-    let outOfStockTextContent = await page.$eval(outOfStockDivSelector, el => el.textContext);
+    let outOfStockTextContent = await page.$eval(
+      outOfStockDivSelector,
+      (el) => {
+        return el.textContent;
+      }
+    );
     if(outOfStockTextContent) {
-        console.log(outOfStockTextContent);
+      outOfStockTextContent = outOfStockTextContent.trim();
+      console.log(outOfStockTextContent);
     }
   } catch(e) {
         console.log('error!')
